7195b071461ae8d6e294d3a7dfe76029e525b84f
marcelpinotti <marcel.pinotti@gmail.com>
2022-08-06 22:23:20 -0300

    Criando projeto para estudo da anotacao JsonViews e DTOS

---
+++ src/main/java/br/com/marcelpinotti/jsonviewsexample/JsonViewsExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.jsonviewsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonViewsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonViewsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/marcelpinotti/jsonviewsexample/JsonViewsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.jsonviewsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonViewsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
